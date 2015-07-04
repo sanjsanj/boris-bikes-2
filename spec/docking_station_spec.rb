4e1220b4b95e5217950005504ff1657d93cc753b
sanjsanj <sanjsanj@hotmail.com>
2015-07-04 20:41:24 +0100

    docking station errors when no bike to release

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,7 +6,18 @@ describe DockingStation do
   end
 
   it 'releases working bikes' do
+    subject.dock Bike.new
     bike = subject.release_bike
     expect(bike).to be_working
+  end
+
+  it 'responds to dock with 1 arg' do
+    expect(subject).to respond_to(:dock).with(1).argument
+  end
+
+  describe 'release_bike' do
+    it 'raises an error when there are no bikes available' do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
   end
 end
